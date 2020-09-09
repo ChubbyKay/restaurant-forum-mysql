5280e303c42137919ba5f3b223efeba0c3746dbf
Kay <kahachang@gmail.com>
2020-09-09 13:06:00 +0800

    add authenticated and authenticatedAdmin function

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,31 @@ const adminController = require('../controllers/adminController')
 const userController = require('../controllers/userController')
 
 module.exports = (app, passport) => {
+  // 檢查使用者是否登入，若無導回登入頁
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/signin')
+  }
+  // 檢查是否具備管理員身份
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) {
+        return next()
+      }
+      return res.redirect('/')
+    }
+    res.redirect('/signin')
+  }
   // 瀏覽首頁會導入 restaurants 的頁面
-  app.get('/', (req, res) => res.redirect('restaurants'))
+  app.get('/', authenticated, (req, res) => res.redirect('restaurants'))
   // 瀏覽 /restaurants 的頁面，則交由 restController.getRestaurants 處理
-  app.get('/restaurants', restController.getRestaurants)
+  app.get('/restaurants', authenticated, restController.getRestaurants)
 
   // admin 的路由設定
-  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
-  app.get('/admin/restaurants', adminController.getRestaurants)
+  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
 
   // user 的路由設定
   app.get('/signup', userController.signUpPage)
