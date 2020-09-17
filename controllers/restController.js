25e17dd7a255fbcdd4f34ca73566d5f5b76d1032
Kay <kahachang@gmail.com>
2020-09-17 15:11:13 +0800

    add pagination

--- controllers/restController.js
+++ controllers/restController.js
@@ -3,35 +3,53 @@ const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
 
+const pageLimit = 10
+
 // restController 是一個物件
 const restController = {
   // getRestaurants 是 restController 的其中一個屬性，也是個函式，負責瀏覽餐廳頁面
   getRestaurants: (req, res) => {
+    let offset = 0
     // whereQuery 是要傳給 findAll 的參數，故須包裝成物件格式
     let whereQuery = {}
     // categoryId 是要放進 WhereQuery 的內容
     let categoryId = ''
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
     if (req.query.categoryId) {
       // req.query.categoryId 回傳值為字串，故用 Number 轉為數字格式
       categoryId = Number(req.query.categoryId)
       whereQuery['CategoryId'] = categoryId
     }
-    Restaurant.findAll({ include: Category, where: whereQuery }).then(restaurants => {
+    Restaurant.findAndCountAll({ include: Category, where: whereQuery, offset: offset, limit: pageLimit }).then(result => {
+      // pagination
+      let page = Number(req.query.page) || 1
+      let pages = Math.ceil(result.count / pageLimit)
+      let totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+      let prev = page - 1 < 1 ? 1 : page - 1
+      let next = page + 1 > pages ? pages : page + 1
       // 因 restaurant 的資料是陣列，需搭配 map 處理，map 處理完會產生新陣列，用變數 data 接住回傳值，這時應該會寫成 const data = restaurant.map(r => {r.description = r.description.substring(0,50) return r})
-      const data = restaurants.map(r => ({
+      // console.log('result', result)
+      const data = result.rows.map(r => ({
         // 展開餐廳資料(sequelize 回傳值是[Restaurant {dataValues:{...}}]
         ...r.dataValues,
         // 複寫 description 內容 (substring 用來指定文字限度)
         description: r.dataValues.description.substring(0, 50),
         categoryName: r.Category.name
       }))
-      Category.findAll({ raw: true, nest: true }).then(categories => {
-        return res.render('restaurants', {
-          restaurants: data,
-          categories: categories,
-          categoryId: categoryId
+      Category.findAll({ raw: true, nest: true })
+        .then(categories => {
+          return res.render('restaurants', {
+            restaurants: data,
+            categories: categories,
+            categoryId: categoryId,
+            page: page,
+            totalPage: totalPage,
+            prev: prev,
+            next: next
+          })
         })
-      })
     })
   },
   getRestaurant: (req, res) => {
