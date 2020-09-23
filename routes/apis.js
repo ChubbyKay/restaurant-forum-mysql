b25bc73a7966a1a1be2e3f27afe88919b4a6a920
Kay <kahachang@gmail.com>
2020-09-23 22:35:05 +0800

    refactor adminController.postRestaurant

--- routes/apis.js
+++ routes/apis.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
+const multer = require('multer')
+const upload = multer({ dest: 'temp/' })
+
 const adminController = require('../controllers/api/adminController.js')
 const categoryController = require('../controllers/api/categoryController.js')
 
 // restaurant 相關路由
 router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
+router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
 router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
 
 // category 相關路由
